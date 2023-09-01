ec96f4c8b08e0c872f5c1e755f73f085218d263a
hwtheowl <jegal7000@gmail.com>
2023-09-01 14:59:50 +0900

    4th

---
+++ java jungsuk/ch01/src/Hello.java
@@ -0,0 +1,16 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// ctrl+shift+L 단축키 전체 목록보기
+		// ctrl+ +, -
+		// ctrl+DS한 줄 삭제
+		// ctrl+aS+down 행단위 복사
+		// alt+shift+A 멀티컬럼 편집
+		// alt+up, down 행단위 이동
+		// ctrl+i 자동 들여쓰기
+		System.out.println("Hello,world.");
+
+	}
+
+}
